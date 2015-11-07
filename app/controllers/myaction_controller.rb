3b8107e38aa26fd280a0487131d54bd53bcd5e3d
Hakozaru <dragontails1025@gmail.com>
2015-11-07 16:48:10 +0900

    first commit

---
+++ app/controllers/myaction_controller.rb
@@ -0,0 +1,10 @@
+class MyactionController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def world
+  	render text:"Ruby world"
+  end
+
+end
