44591813c413e6ed13e7dfa309a2a15ae85f9a6c
Hakozaru <dragontails1025@gmail.com>
2015-11-14 17:28:14 +0900

    コメントを作成してエントリーと関連付けしました

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114072303) do
+ActiveRecord::Schema.define(version: 20151114082343) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "body"
+    t.integer  "entry_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string   "name"
